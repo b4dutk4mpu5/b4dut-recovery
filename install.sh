d6c464350b0452e6a30d20b01ddb8aecb51461f9
b4dutk4mpu5 <55218680+b4dutk4mpu5@users.noreply.github.com>
2019-09-16 15:42:00 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/data/data/com.termux/files/home/bash
+apt update && apt upgrade -y
+apt install python2 -y
+easy_install requesocks
+ -y
+pip install requesocks
+ -y
+apt install figlet -y
+yellow='\e[93m'
+blue='\e[1;34m'
+green='\e[0;23m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+PS1=$purple"b4dutk4mpu5$yellow\w# "
+clear
+echo -e $red'============================================================'
+echo -e $yellow
+figlet "    <<(*_*)>>  ACT"
+
+echo -e $cyan'============================================================'
+echo -e $red[+]$purple Welcome $red[+] $cyan Termux Indonesia$red We Are$yellow Radar 404 Team $red[+]
+echo -e '############################################################'
+python2 fb
